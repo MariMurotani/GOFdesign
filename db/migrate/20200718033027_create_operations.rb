ab3522bdc717502212323e3034ef971d9ce31695
Mari Murotani <muromari5123@gmail.com>
2020-07-18 23:53:42 +0900

    Fixed forbidden field name in db

--- db/migrate/20200718033027_create_operations.rb
+++ db/migrate/20200718033027_create_operations.rb
@@ -2,7 +2,7 @@ class CreateOperations < ActiveRecord::Migration[5.2]
   def change
     create_table :operations do |t|
       t.string :name
-      t.integer :type, null: :false, limit: 3
+      t.integer :sub_type, null: :false, limit: 3
       t.timestamps
     end
   end
