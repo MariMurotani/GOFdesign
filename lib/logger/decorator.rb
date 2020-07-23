bb0918244a3fcefbacc035e18cb7efc3bf4fd065
Mari Murotani <muromari5123@gmail.com>
2020-07-23 15:35:33 +0900

    decoratorでdelegateのリファクタリング

--- lib/logger/decorator.rb
+++ lib/logger/decorator.rb
@@ -1,18 +1,9 @@
+require 'forwardable'
 class Decorator
+  extend Forwardable
+  delegate [:format_line, :pos, :execute] => :@real_formatter
   def initialize(real_formatter)
     @real_formatter = real_formatter
     @result = []
-  end
-  def account
-    @real_formatter.account
-  end
-  def format_line(row)
-    @real_formatter.format_line(row)
-  end
-  def pos
-    @real_formatter.pos
-  end
-  def execute
-    @real_formatter.execute
   end
 end
