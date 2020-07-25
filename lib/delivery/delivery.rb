4ad2dfeaa7f65228a5e68e9c0cb40aa272167665
Mari Murotani <muromari5123@gmail.com>
2020-07-25 13:10:40 +0900

    コミット漏れコミット

--- lib/delivery/delivery.rb
+++ lib/delivery/delivery.rb
@@ -1,20 +1,20 @@
 require "#{Rails.root}/lib/delivery/areas/kanto.rb"
-require "#{Rails.root}/lib/delivery/areas/okinawa.rb"
+  require "#{Rails.root}/lib/delivery/areas/okinawa.rb"
 
-module Delivery
-  class Delivery < Step
-    def initialize(postal_code)
-      @postal_code = postal_code
-      @name = "配送時間"
-    end
-    def get_time_required
-      if @postal_code == "1000000"
-        Kanto.new.get_time_required
-      elsif @postal_code == "9000000"
-        Okinawa.new.get_time_required
-      else
-        3
+  module Delivery
+    class Delivery < Step
+      def initialize(postal_code)
+        @postal_code = postal_code
+        @name = "配送時間"
+      end
+      def get_time_required
+        if @postal_code == "1000000"
+          Kanto.new.get_time_required
+        elsif @postal_code == "9000000"
+          Okinawa.new.get_time_required
+        else
+          3
+        end
       end
     end
   end
-end
