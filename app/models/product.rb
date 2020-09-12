78636083b63b474db347ca84ce19468fa0bc90ea
Mari Murotani <muromari5123@gmail.com>
2020-09-12 15:08:39 +0900

    has many through ordersの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   include ActiveModel::Serialization
-  has_many :order
+  has_many :ordered_products
+  has_many :order, through: :ordered_products
   has_one :stock
   scope :with_stock, -> { joins(:stock) }
   def to_json
