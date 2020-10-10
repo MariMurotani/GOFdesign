c3f2d7514c645b1d0e02985b2c19dd4d015e2e43
Mari Murotani <muromari5123@gmail.com>
2020-10-10 18:21:40 +0900

    to_jsonなくす

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,4 @@ class Product < ApplicationRecord
   has_many :order, through: :ordered_products
   has_one :stock
   scope :with_stock, -> { joins(:stock) }
-  def to_json
-    result = self.attributes
-    result["ec_stock_amount"] = self&.stock&.ec_stock_amount
-    result["store_stock_amount"] = self&.stock&.store_stock_amount
-    result
-  end
 end
