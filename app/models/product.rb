55ea1fe499e1ac50ab392c79f700f20313684863
Mari Murotani <muromari5123@gmail.com>
2020-10-10 18:14:27 +0900

    to_jsonの書き方修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,9 @@ class Product < ApplicationRecord
   has_one :stock
   scope :with_stock, -> { joins(:stock) }
   def to_json
-    result = self
-    result[:ec_stock_amount] = self.stock.ec_stock_amount
-    result[:store_stock_amount] = self.stock.store_stock_amount
+    result = self.attributes
+    result["ec_stock_amount"] = self&.stock&.ec_stock_amount
+    result["store_stock_amount"] = self&.stock&.store_stock_amount
+    result
   end
 end
