c7509f921b01179bf8b8b37dbdc9c1341bed59b1
Mari Murotani <muromari5123@gmail.com>
2020-10-11 13:37:26 +0900

    変数のスコープを修正

--- lib/order/order_service.rb
+++ lib/order/order_service.rb
@@ -4,18 +4,18 @@ class OrderService
     @account = account
     if order_id.present?
       @order = Order.find(order_id)
-      @ordered_products = @order.ordered_product
+      ordered_products = @order.ordered_product
       @order_bill = @order.order_bill
     else
       @order = Order.new({
          account: @account
        })
-      @ordered_products = []
+      ordered_products = []
       @order_bill = nil
     end
   end
   def add_item(product_id, amount)
-    @ordered_products << OrderedProduct.new({product_id: product_id, quantity: amount})
+    ordered_products << OrderedProduct.new({product_id: product_id, quantity: amount})
   end
   def set_delivery_method(method)
     @order.delivery_method = Order.delivery_methods["mail"] if method == 'mail'
@@ -24,15 +24,15 @@ class OrderService
   def save!
     ActiveRecord::Base.transaction do
       @order.save!
-      @ordered_products.each do | ordered_product |
-        delivery_date_time = DeliveryTimeEstimate.new(ordered_product.product, ordered_product.quantity,@account.address.last.postal_code).delivery_date_time
+      ordered_products.each do | ordered_product |
+        delivery_date_time = DeliveryTimeEstimate.new(ordered_product.product, ordered_product.quantity,@account.address.last.postal_code).estimate_delivery_date_time
         ordered_product.attributes = {
           order: @order,
           expected_delivery_date: delivery_date_time
         }
         ordered_product.save
       end
-      total_price = Order::Billing::Price.new(@ordered_products.map(&:total_price).sum)
+      total_price = Order::Billing::Price.new(ordered_products.map(&:total_price).sum)
       discount_price = Order::Billing::Price.new(total_price.get_operand_price*0.13)
       shipping_fee = Order::Billing::Price.new(300)
       discounted_price = Order::Billing::Minus.new(total_price, discount_price).execute
