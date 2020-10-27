c00d214d81bd4df75add6ac4f44dadb011674d65
Mari Murotani <muromari5123@gmail.com>
2020-10-27 13:52:25 +0900

    サブタイプを追加

--- app/models/concerns/account_factory.rb
+++ app/models/concerns/account_factory.rb
@@ -2,8 +2,10 @@ class AccountFactory < AbstractFactory
   def self.create(type)
     if type == 'system'
       SystemAccount.new
-    else
-      Account.new
+    elsif type == 'admin'
+      AdminAccount.new
+    elsif type == 'shopper'
+      ShopperAccount.new
     end
   end
 end
