dcd481b6edc536b3f0d6ce29de98a83c5970f8f2
Mari Murotani <muromari5123@gmail.com>
2021-01-30 12:02:12 +0900

    リファクタリングしたので修正

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,16 +5,6 @@ class Account < ApplicationRecord
   has_many :addresses, dependent: :destroy
   enum account_type: { systemadmin: 0, administrator: 1, shopper: 2 }
   scope :system_user, -> { where(account_type: Account.account_types[:systemadmin]).last }
-  def self.create_user(target)
-    case target
-    when 'SYSTEM'
-      self.new({ account_type: Account.account_types[:systemadmin] })
-    when 'ADMIN'
-      self.new({ account_type: Account.account_types[:administrator] })
-    when 'SHOPPER'
-      self.new({ account_type: Account.account_types[:shopper] })
-    end
-  end
 
   def attributes
     {
